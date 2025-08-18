25add507fc3026eb4e54fafeb8192f2a45f740a0
ModderCodder <1modder.path1@gmail.com>
2025-08-18 13:38:11 +0300

    v1.0

--- FitnessTrackerBack/Program.cs
+++ FitnessTrackerBack/Program.cs
@@ -2,11 +2,26 @@
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using System.Text.Json;
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions{
+		Args = args,
+		WebRootPath = "wwwroot",
+		ContentRootPath = Directory.GetCurrentDirectory(),
+		ApplicationName = "FitnessTrackerBack"
+		});
+
+builder.WebHost.UseUrls("http://0.0.0.0:5024");
+
+builder.WebHost.ConfigureKestrel(serverOptions => {
+    serverOptions.ListenAnyIP(5024);
+});
 
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+		
+		
+		
+
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
     ?? "Host=localhost;Port=5432;Database=fitness_tracker;Username=postgres;Password=12345";
@@ -18,8 +33,18 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+
+    app.UseSwaggerUI(options => {
+		
+			options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+			options.RoutePrefix = "swagger";
+    });                                                              
 }
+
+
+app.MapGet("/", () => "FitnessTracker API is running!");
+app.MapGet("/ping", () => "pong");
+
 
 app.MapGet("/activities/{username}", async (string username, AppDbContext db) =>
 {
